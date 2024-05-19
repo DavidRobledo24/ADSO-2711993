ba528cc6962e744f2afcf6e69f8ee34aa8c7381e
DavidRobledo24 <darobledo98@soy.sena.edu.co>
2024-05-18 19:36:58 -0500

    editar personas api

--- API-PERSONAS/js/script.js
+++ API-PERSONAS/js/script.js
@@ -1,9 +1,17 @@
 const filas = document.getElementById('filas');
 const formInsertar = document.getElementById('formInsertar');
-let myModal = new boostrap.Modal(document.getElementById('modalInsetar'));
+const formEditar = document.getElementById('formEditar');
+let myModal = new bootstrap.Modal(document.getElementById('modalInsetar'));
+let myModal2 = new bootstrap.Modal(document.getElementById('modalEditar'));
+
 formInsertar.addEventListener("submit", function(event){
     event.preventDefault();
     agregarPersona();
+});
+
+document.getElementById('botonEditar').addEventListener("click", function(event){
+    event.preventDefault();
+    editarPersona();
 });
 
 llamarPersonas();
@@ -11,24 +19,36 @@ llamarPersonas();
 function llamarPersonas(){
     let endPoint = "https://codetesthub.com/API/Obtener.php";
     
-    fetch(endPoint) .then(resp => resp.json())
+    fetch(endPoint).then(resp => resp.json())
     .then(data => {
-        for(var i = 0; i<data.length;i++){
-                let temp = `<tr>
-                                <td>${i+1}</td>
-                                <td>${data[i].cedula}</td>
-                                <td>${data[i].nombres}</td>
-                                <td>${data[i].apellidos}</td>
-                                <td class="text-center">${data[i].telefono}</td>
-                                <td class="text-center">${data[i].direccion}</td>
-                                <td class="text-center">${data[i].email}</td>
-                                <td>
-                                    <button class="btn btn-danger d-flex justify-content-center" onClick="eliminarPersona(${data[i].cedula})">X</button>
-                                </td>
-                            <tr>`;
+        filas.innerHTML = "";
+        for(var i = 0; i < data.length; i++){
+            let temp = `<tr>
+                            <td>${i + 1}</td>
+                            <td>${data[i].cedula}</td>
+                            <td>${data[i].nombres}</td>
+                            <td>${data[i].apellidos}</td>
+                            <td class="text-center">${data[i].telefono}</td>
+                            <td class="text-center">${data[i].direccion}</td>
+                            <td class="text-center">${data[i].email}</td>
+                            <td>
+                                <button class="btn btn-danger" onClick="eliminarPersona(${data[i].cedula})">X</button>
+                                <button class="btn btn-primary mx-2" data-bs-toggle="modal"
+                                data-bs-target="#modalEditar" onClick="cargarDatosEditar(${data[i].cedula}, '${data[i].nombres}', '${data[i].apellidos}', '${data[i].telefono}', '${data[i].direccion}', '${data[i].email}')">E</button>
+                            </td>
+                        </tr>`;
             filas.innerHTML += temp;
         }
-    });      
+    });
+}
+
+function cargarDatosEditar(cedula, nombres, apellidos, telefono, direccion, email) {
+    document.getElementById('cedulaEditar').value = cedula;
+    document.getElementById('nombresEditar').value = nombres;
+    document.getElementById('apellidosEditar').value = apellidos;
+    document.getElementById('telefonoEditar').value = telefono;
+    document.getElementById('direccionEditar').value = direccion;
+    document.getElementById('emailEditar').value = email;
 }
 
 function agregarPersona(){
@@ -36,9 +56,9 @@ function agregarPersona(){
   
     let configuracion = {
         method: "POST",
-        Headers : {
-                "Accept" : "application/json",
-            },
+        headers: {
+            "Accept": "application/json"
+        },
         body: datos
     };
 
@@ -49,7 +69,6 @@ function agregarPersona(){
         console.log(data);
 
         if(data.status){
-            
             myModal.hide();
             llamarPersonas();
         }else{
@@ -64,9 +83,9 @@ function eliminarPersona(cedula){
 
     let configuracion = {
         method: "POST",
-        Headers : {
-                "Accept" : "application/json",
-            },
+        headers: {
+            "Accept": "application/json"
+        },
         body: datos
     };
 
@@ -82,9 +101,30 @@ function eliminarPersona(cedula){
             alert("Error al eliminar");
         }
     });
-
 }
 
 function editarPersona(){
+    let datos = new FormData(formEditar);
 
+    let configuracion = {
+        method: "POST",
+        headers: {
+            "Accept": "application/json"
+        },
+        body: datos
+    };
+
+    fetch("https://codetesthub.com/API/Actualizar.php", configuracion)
+    .then(resp => resp.json())
+    .then(data => {
+        console.log("La API responde con: ");
+        console.log(data);
+
+        if(data.status){
+            myModal2.hide();
+            llamarPersonas();
+        }else{
+            alert("Error al editar");
+        }
+    });
 }
